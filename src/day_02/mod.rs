95a518a4082f6b6cae134fb30015a33beff060ea
Gábor Marczi <marczi.gabor@outlook.com>
2022-12-22 16:04:10 +0100

    day 2

--- src/day_02/mod.rs
+++ src/day_02/mod.rs
@@ -38,3 +38,53 @@ pub fn day_02_01() -> i32 {
 
     point
 }
+
+pub fn day_02_02() -> i32 {
+    let file_path = "src/day_02/data.txt";
+    let contents;
+    let mut point = 0;
+    let mut selected;
+    contents = utils::read_file(file_path);
+
+    let mut dict = HashMap::new();
+    dict.insert( "A", 1 );
+    dict.insert( "B", 2 );
+    dict.insert( "C", 3 );
+
+    for line in contents.lines().into_iter() {
+        let v: Vec<&str> = line.split(' ').collect();
+        
+        // X = lose
+        // Y = draw
+        // Z = win
+        selected = "A";
+        if v[1].eq("X") {
+
+            match v[0] {
+                "A" => selected = "C",
+                "B" => selected = "A",
+                "C" => selected = "B",
+                &_ => todo!()
+            }
+
+        } else if v[1].eq("Y") {
+            
+            selected = v[0];
+            point += 3;
+
+        } else if v[1].eq("Z"){
+            
+            match v[0] {
+                "A" => selected = "B",
+                "B" => selected = "C",
+                "C" => selected = "A",
+                &_ => todo!()
+            }
+            point += 6;
+        }
+
+        point += *dict.get(selected).unwrap();
+    }
+
+    point
+}
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ fn main() {
     
     println!("day 01 - 01: {}", day_01::day_01_01());
     println!("day 01 - 02: {}", day_01::day_01_02());
+    
     println!("day 02 - 01: {}", day_02::day_02_01());
-    //println!("day 01 - 02: {}", day_01::day_01_02());
+    println!("day 01 - 02: {}", day_02::day_02_02());
 }
 
 
