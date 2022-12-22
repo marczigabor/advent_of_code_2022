e0394dd82967de93c5001fe90293eb807c768606
Gábor Marczi <43849602+marczigabor@users.noreply.github.com>
2022-12-22 07:17:08 +0000

    initial

---
+++ src/day_01/mod.rs
@@ -0,0 +1,53 @@
+use std::fs;
+
+pub fn day_01_01() -> i32 {
+    let file_path = "src/day_01/data.txt";
+    let contents;
+    let mut max = 0;
+    let mut local_max = 0;
+
+    contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    for line in contents.lines().into_iter() {
+        if line.is_empty() {
+            if local_max > max {
+                max = local_max;
+            }
+            local_max = 0;
+        } else {
+            local_max += line.parse::<i32>().unwrap();
+        }
+    }
+
+    if local_max > max {
+        max = local_max;
+    }
+
+    max
+}
+
+pub fn day_01_02() -> i32 {
+    let file_path = "src/day_01/data.txt";
+    let contents;
+    let mut sum = 0;
+    let mut vec = Vec::new();
+
+    contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    for line in contents.lines().into_iter() {
+        if line.is_empty() {
+            vec.push(sum);
+            sum = 0;
+        } else {
+            sum += line.parse::<i32>().unwrap();
+        }
+    }
+
+    if sum > 0 {
+        vec.push(sum);
+    }
+
+    vec.sort_by(|a, b| b.cmp(a));
+    vec.iter().take(3).sum()
+
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod day_01;
+
+fn main() {
+    
+    println!("day 01 - 01: {}", day_01::day_01_01());
+    println!("day 01 - 02: {}", day_01::day_01_02());
+
+}
+
+
